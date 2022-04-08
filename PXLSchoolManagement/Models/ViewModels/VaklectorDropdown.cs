f94073bffb1b965d54611e16475ad2b7825ace48
Wouter <wtrvngl@gmail.com>
2022-04-09 00:19:07 +0200

    Vaklector ViewModel added + DropDown class

---
+++ PXLSchoolManagement/Models/ViewModels/VaklectorDropdown.cs
@@ -0,0 +1,10 @@
+﻿namespace PXLSchoolManagement.Models.ViewModels
+{
+    public class VaklectorDropdown
+    {
+        public int LectorId { get; set; }
+        public string Naam { get; set; }
+        public string Voornaam { get; set; }
+        public string VolledigeNaam => Voornaam + " " + Naam;
+    }
+}
