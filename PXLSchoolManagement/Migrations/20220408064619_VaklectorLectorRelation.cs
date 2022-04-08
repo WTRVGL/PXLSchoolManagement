766bf734e6ec64ba5351a9769079f8826acd52fc
Wouter <wtrvngl@gmail.com>
2022-04-09 00:18:14 +0200

    Migration-added

---
+++ PXLSchoolManagement/Migrations/20220408064619_VaklectorLectorRelation.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PXLSchoolManagement.Migrations
+{
+    public partial class VaklectorLectorRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Vaklectoren_LectorId",
+                table: "Vaklectoren");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vaklectoren_LectorId",
+                table: "Vaklectoren",
+                column: "LectorId",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Vaklectoren_LectorId",
+                table: "Vaklectoren");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Vaklectoren_LectorId",
+                table: "Vaklectoren",
+                column: "LectorId");
+        }
+    }
+}
