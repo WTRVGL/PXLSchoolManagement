3a2c64e9981385397eeda6c1f31d4bbf51be0b1f
Wouter <wtrvngl@gmail.com>
2022-04-09 20:36:04 +0200

    model changed

--- PXLSchoolManagement/Models/Inschrijving.cs
+++ PXLSchoolManagement/Models/Inschrijving.cs
@@ -6,8 +6,10 @@ namespace PXLSchoolManagement.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public List<Student> Studenten { get; set; }
         public List<Vaklector> Vaklectors{ get; set; }
+        [Required]
         public int VakId { get; set; }
         public Vak Vak { get; set; }
         public int AcademiejaarId { get; set; }
