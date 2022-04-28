67d78263f8334ea101ec82ebc65c40d973156215
Wouter Vangeel <wtrvngl@gmail.com>
2022-04-28 09:38:52 +0200

    rm cache

--- PXLSchoolManagement/Models/Academiejaar.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PXLSchoolManagement.Models
-{
-    public class Academiejaar
-    {
-        public int AcademiejaarId { get; set; }
-        public DateTime Startdatum { get; set; }
-        public List<Inschrijving> Inschrijvingen { get; set; }
-    }
-}
