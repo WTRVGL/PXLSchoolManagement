74ea3fea564771820b3c75d018c8d737cc620611
Wouter <wtrvngl@gmail.com>
2022-05-08 14:54:38 +0200

    Layout redirect to /manage

--- PXLSchoolManagement/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ PXLSchoolManagement/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -37,7 +37,7 @@ namespace PXLSchoolManagement.Areas.Identity.Pages.Account
             {
                 // This needs to be a redirect so that the browser performs a new
                 // request and the identity for the user gets updated.
-                return RedirectToPage();
+                return RedirectToPage("index");
             }
         }
     }
