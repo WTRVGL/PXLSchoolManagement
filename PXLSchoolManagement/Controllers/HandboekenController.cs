fef427949f32e722acd8f1c573d5103dda2a7630
Wouter Vangeel <wtrvngl@gmail.com>
2022-05-15 05:33:21 +0200

    Scaffold Handboeken

---
+++ PXLSchoolManagement/Controllers/HandboekenController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PXLSchoolManagement.Controllers
+{
+    public class HandboekenController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
