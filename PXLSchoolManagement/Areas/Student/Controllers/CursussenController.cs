782c56e2fa71b11f4ead037a62c30a34521a2649
Wouter <wtrvngl@gmail.com>
2022-05-28 13:04:20 +0200

    Cursus Detail action method +  Vieww

--- PXLSchoolManagement/Areas/Student/Controllers/CursussenController.cs
+++ PXLSchoolManagement/Areas/Student/Controllers/CursussenController.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PXLSchoolManagement.Areas.Student.Models;
 using PXLSchoolManagement.Data;
+using PXLSchoolManagement.Models;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace PXLSchoolManagement.Areas.Student.Controllers
 {
@@ -11,10 +14,12 @@ namespace PXLSchoolManagement.Areas.Student.Controllers
     public class CursussenController : Controller
     {
         private readonly DataContext _context;
+        private readonly UserManager<Gebruiker> _userManager;
 
-        public CursussenController(DataContext context)
+        public CursussenController(DataContext context, UserManager<Gebruiker> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         [Authorize(Roles = "Admin,Student")]
@@ -30,6 +35,30 @@ namespace PXLSchoolManagement.Areas.Student.Controllers
                     .ThenInclude(l => l.Gebruiker)
                 .ToList();
 
+            return View(vm);
+        }
+
+        [Authorize(Roles = "Admin,Student")]
+        public async Task<IActionResult> Detail(int? id)
+        {
+            var user = await _userManager.GetUserAsync(User);
+            var vm = new StudentCursusDetailViewModel();
+            vm.Cursus = _context.Inschrijvingen
+                .Include(i => i.Vak)
+                    .ThenInclude(v => v.Handboeken)
+                .Include(i => i.Academiejaar)
+                .Include(i => i.Studenten)
+                    .ThenInclude(s => s.Gebruiker)
+                .Include(i => i.Vaklectors)
+                    .ThenInclude(v => v.Lector)
+                    .ThenInclude(l => l.Gebruiker)
+                .FirstOrDefault(i => i.Id == id);
+
+            vm.IsIngeschreven = _context.Inschrijvingen
+                .Where(i => i.Id == id)
+                .SelectMany(i => i.Studenten)
+                .Any(s => s.GebruikerId == user.Id);
+            
             return View(vm);
         }
 
---
+++ PXLSchoolManagement/Areas/Student/Models/StudentCursusDetailViewModel.cs
@@ -0,0 +1,11 @@
+﻿using PXLSchoolManagement.Models;
+using System.Collections.Generic;
+
+namespace PXLSchoolManagement.Areas.Student.Models
+{
+    public class StudentCursusDetailViewModel
+    {
+        public Inschrijving Cursus { get; set; }
+        public bool IsIngeschreven { get; set; }
+    }
+}
