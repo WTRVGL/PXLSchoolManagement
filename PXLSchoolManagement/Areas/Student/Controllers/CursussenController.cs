4f7376536b9a785a90e147f0ad26bde2ffae76dc
Wouter <wtrvngl@gmail.com>
2022-05-28 10:53:53 +0200

    VM created + controller stuff

--- PXLSchoolManagement/Areas/Student/Controllers/CursussenController.cs
+++ PXLSchoolManagement/Areas/Student/Controllers/CursussenController.cs
@@ -1,21 +1,36 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PXLSchoolManagement.Areas.Student.Models;
+using PXLSchoolManagement.Data;
+using System.Linq;
 
 namespace PXLSchoolManagement.Areas.Student.Controllers
 {
     [Area("Student")]
     public class CursussenController : Controller
     {
-        [Authorize(Roles ="Admin,Student")]
-        public IActionResult Index()
+        private readonly DataContext _context;
+
+        public CursussenController(DataContext context)
         {
-            return View();
+            _context = context;
         }
 
         [Authorize(Roles = "Admin,Student")]
-        public IActionResult Cursussen()
+        public IActionResult Index()
         {
-            return View();
+            var vm = new StudentCursussenIndexViewModel();
+            vm.Cursussen = _context.Inschrijvingen
+                .Include(i => i.Vak)
+                .Include(i => i.Academiejaar)
+                .Include(i => i.Studenten)
+                .Include(i => i.Vaklectors)
+                    .ThenInclude(v => v.Lector)
+                    .ThenInclude(l => l.Gebruiker)
+                .ToList();
+
+            return View(vm);
         }
 
     }
